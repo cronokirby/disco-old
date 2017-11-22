eeb08e976e9fd3b5c9f8a0e5d7791b418ad8c274
Lúcás Meier <cronokirby@gmail.com>
2017-11-22 16:56:50 +0100

    adds basic structure

--- src/Network/Disco.hs
+++ src/Network/Disco.hs
@@ -1,4 +1,9 @@
-module Network.Disco where
+module Network.Disco
+    ( module Network.Disco.Types )
+where
+
+import           Network.Disco.Types
+
 
 someFunc :: IO
 someFunc = putStrLn "Hello World"
---
+++ src/Network/Disco/Types.hs
@@ -0,0 +1,1 @@
+module Network.Disco.Types () where
